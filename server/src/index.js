9f761cb8121fb47082000133c036d6996cb0e5b3
tamas <tamas@giffgaff.co.uk>
2018-10-24 17:01:34 +0100

    split schemas

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,31 @@
 const { GraphQLServer } = require('graphql-yoga');
+const fs = require('fs');
+const { gql } = require('apollo-server-express');
+const glob = require('glob');
+const path = require('path');
+
 const Api = require('./service/api');
 const resolvers = require('./resolvers');
+
+const basePath = path.join(__dirname, './models/');
+const schemasPath = glob.sync(`${basePath}**/*.graphql`);
+
+const Query =gql`
+  type Query {
+    _empty: String
+  }
+`;
+
+const typeDefs = [
+  Query,
+  ...schemasPath
+    .map(schema => gql(fs.readFileSync(schema, 'utf8'))),
+];
 
 const context = { api: Api };
 const options = { port: 4000 };
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs,
   resolvers,
   context
 });
